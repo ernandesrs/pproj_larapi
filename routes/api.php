fcbc1f0f5d52456219b1cb76feffb97e6d389add
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-05-18 16:38:48 -0400

    Api routes v1

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,27 @@
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', fn() => \App\Helpers\ApiResponse::success());
-
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'v1'
 ], function () {
 
+    Route::get('/test', fn() => \App\Helpers\ApiResponse::success());
+
     Route::group([
-        'middleware' => 'guest:sanctum'
+        'prefix' => 'auth'
     ], function () {
-        Route::post('/register', [RegisterController::class, 'store'])->name('auth.register');
-        Route::get('/verify', [RegisterController::class, 'verify'])
-            ->name('auth.verify');
-        Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
+
+        Route::group([
+            'middleware' => 'guest:sanctum'
+        ], function () {
+            Route::post('/register', [RegisterController::class, 'store'])->name('auth.register');
+            Route::get('/verify', [RegisterController::class, 'verify'])
+                ->name('auth.verify');
+            Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
+        });
+
     });
 
 });

75e5880f20d2ee90113bad7bb864c627005ff05f
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-05-24 09:19:39 -0400

    User policy updates(check user->id and model->id)

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -8,6 +8,34 @@ use Illuminate\Auth\Access\Response;
 class UserPolicy extends BasePolicy
 {
     /**
+     * Update
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function update(User $user, \Illuminate\Database\Eloquent\Model $model): bool
+    {
+        if (!parent::update($user, $model) || $user->id === $model->id)
+            return false;
+
+        return $user->isSuperuser() ? true : !$model->isAdmin();
+    }
+
+    /**
+     * Update
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function delete(User $user, \Illuminate\Database\Eloquent\Model $model): bool
+    {
+        if (!parent::delete($user, $model) || $user->id === $model->id)
+            return false;
+
+        return $user->isSuperuser() ? true : !$model->isAdmin();
+    }
+
+    /**
      * Promote user
      * @param \App\Models\User $user
      * @param \App\Models\User $model
@@ -15,6 +43,9 @@ class UserPolicy extends BasePolicy
      */
     public function promote(User $user, User $model): bool
     {
+        if ($user->id === $model->id)
+            return false;
+
         return $user->isSuperuser();
     }
 
@@ -26,6 +57,9 @@ class UserPolicy extends BasePolicy
      */
     public function demote(User $user, User $model): bool
     {
+        if ($user->id === $model->id)
+            return false;
+
         return $user->isSuperuser();
     }
 }
